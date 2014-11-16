fbdcaf76e102e279811faf38d28c27be9ce524a1
Nazzanuk <nazzanuk@gmail.com>
2014-11-16 16:16:50 +0000

    Initial screens

--- app/components/data/dataService.js
+++ app/components/data/dataService.js
@@ -11,20 +11,41 @@ app.service('DataService', function () {
 
     that.groups = [
         {
-            'name':'Past Tense',
+            'name':'Basic',
             'lines':[
-                {'spanish': 'tuvé','english':'I had'},
-                {'spanish': 'tenía','english':'I used to have'},
+                {'spanish': ['Mañana', 'Tomorrow1', 'Tomorrow2'],'english':['Tomorrow', 'Tomorrow1', 'Tomorrow2']},
+                {'spanish': ['Hoy'],'english':['Today']},
+                {'spanish': ['Niño'],'english':['Boy']}
             ]
         },
         {
             'name':'Past Tense',
             'lines':[
-                {'spanish': 'tuvé','english':'I had'},
-                {'spanish': 'tenía','english':'I used to have'},
+                {'spanish': ['tuvé'],'english':['I had']},
+                {'spanish': ['tenía'],'english':['I used to have']}
             ]
         }
     ];
+
+    that.groupIndex = 0;
+    that.lineIndex = 0;
+    that.phraseIndex = 0;
+    that.language = 'spanish';
+
+    that.generateLine = function () {
+        that.lineIndex = _.random(0, that.groups[that.groupIndex].length);
+        that.phraseIndex = _.random(0, that.groups[that.groupIndex].lines[that.lineIndex][that.language].length - 1);
+        //that.language = _.random(0,1, that.groups[that.groupIndex][that.lineIndex]);
+    };
+    that.generateLine();
+
+    that.getLine = function () {
+        return that.groups[that.groupIndex].lines[that.lineIndex][that.language][that.phraseIndex];
+    };
+
+    that.getGroup = function () {
+        return that.groups[that.groupIndex];
+    };
 
 
     return that;
---
+++ app/components/train/trainCtrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var app = angular.module('otherTongue');
+
+app.controller('trainCtrl', function ($scope, DataService) {
+    $scope.DataService = DataService;
+
+
+});
